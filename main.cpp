dd225343b75bea646d083241f484c3212f5c9821
Lejing Huang <ohaluminum@users.noreply.github.com>
2020-10-05 17:33:06 -0500

    Create doubly linked list.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,64 @@
 #include "ArgumentManager.h"
 using namespace std;
 
+struct expression
+{
+    string info;
+    expression* prev;
+    expression* next;
+};
+
+class expList
+{
+private:
+    expression* head;
+    expression* tail;
+    int size;
+
+public:
+    //Default constructor
+    class expList()
+    {
+        head = nullptr;
+        tail = nullptr;
+        size = 0;
+    }
+
+    void addAtEnd(string info)
+    {
+        //1.Create a temp expression
+        expression* temp = new expression();
+
+        //2.Fill the data
+        temp->info = info;
+        temp->next = nullptr;
+
+        //3.Update the pointer
+        if (size = 0)
+        {
+            temp->prev = nullptr;
+            head = temp;
+            tail = temp;
+        }
+        else if (size > 0)
+        {
+            tail->next = temp;
+            temp->prev = tail;
+
+            tail = temp;
+        }
+
+        //4.Update the size
+        size++;
+    }
+
+
+
+};
+
+
+//MUNUALLY IMPLEMENT STACK
+
 int main(int argc, char* argv[])
 {
     ArgumentManager am(argc, argv);
